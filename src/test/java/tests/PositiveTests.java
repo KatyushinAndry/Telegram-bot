b721c666a0d52e0f36e57e1f8a7a9313eff227c2
My Name <belkatan@yandex.ru>
2021-04-19 00:43:28 +0300

    telegram bot

---
+++ src/test/java/tests/PositiveTests.java
@@ -0,0 +1,74 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+    @Test
+    @Tag("smoke")
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some4Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some5Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some6Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some7Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some8Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some9Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some10Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some11Test() {
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/tests/SomeMorePositiveTests.java
@@ -0,0 +1,62 @@
+package tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SomeMorePositiveTests {
+    @Test
+    @Tag("smoke")
+    void some1Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some2Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some3Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some4Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some5Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some6Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some7Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some8Test() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Tag("smoke")
+    void some9Test() {
+        assertTrue(true);
+    }
+}
